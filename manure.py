5d47acfe30bef2b0d34b33aa7dc727e0de59ca4d
Dominik Neise <neised@phys.ethz.ch>
2017-09-01 15:06:47 +0200

    default qsub and allow for variable sql_query

--- manure.py
+++ manure.py
@@ -2,11 +2,13 @@ from os import makedirs
 from os.path import join
 from os.path import exists
 from os.path import dirname
+from shutils import which
 from datetime import datetime
 import pandas as pd
 from tqdm import tqdm
 from fact.credentials import create_factdb_engine
 import numpy as np
+import subprocess as sp
 
 OBSERVATION_RUN_KEY = 1
 SQL_QUERY = '''
@@ -17,6 +19,30 @@ FROM
 WHERE
     fRunTypeKey={0}
 '''.format(OBSERVATION_RUN_KEY)
+
+
+def qsub(
+    job,
+    binary_name,
+    args=['input_file_path', 'output_file_path'],
+    queue='fact_medium',
+    o_path=None,
+    e_path=None
+):
+
+    o_path = job[o_path] if o_path is not None else '/dev/null'
+    e_path = job[e_path] if e_path is not None else '/dev/null'
+
+    cmd = [
+        'qsub',
+        '-q', queue,
+        '-o', o_path,
+        '-e', e_path,
+        which(binary_name),
+    ]
+    cmd.extend([job[arg] for arg in args])
+
+    sp.check_output(cmd, stderr=sp.STDOUT)
 
 
 def check_for_input_files(runstatus):
@@ -33,11 +59,11 @@ def check_for_input_files(runstatus):
 
 class RunStatus:
 
-    def __init__(self, path, path_gens):
+    def __init__(self, path, path_generators, sql_query):
         self.path = path
-        self.path_gens = path_gens
+        self.path_generators = path_generators
 
-        runstatus = pd.read_sql(SQL_QUERY, create_factdb_engine())
+        runstatus = pd.read_sql(sql_query, create_factdb_engine())
         if exists(self.path):
             old_runstatus = pd.read_csv(self.path)
             old_runstatus['submitted_at'] = pd.to_datetime(old_runstatus.submitted_at)
@@ -69,7 +95,7 @@ class RunStatus:
             desc='makings paths',
             total=len(self.runstatus)
         ):
-            for name, path_gen in self.path_gens.items():
+            for name, path_gen in self.path_generators.items():
                 self.runstatus.set_value(
                     row.Index,
                     name,
@@ -78,19 +104,20 @@ class RunStatus:
 
     def _remove_paths(self):
         self.runstatus.drop(
-            labels=self.path_gens.keys(),
+            labels=self.path_generators.keys(),
             axis=1,
             inplace=True,
         )
 
 
 def production_main(
-        path_gens,
-        function_to_call_with_job,
+        path_generators,
+        submission,
         runstatus_path,
-        out_dir
+        sql_query=None
 ):
-    with RunStatus(runstatus_path, path_gens) as runstatus:
+    sql_query = sql_query if sql_query is not None else SQL_QUERY
+    with RunStatus(runstatus_path, path_generators, sql_query) as runstatus:
 
         runstatus['input_file_exists'] = check_for_input_files(runstatus)
 
@@ -105,6 +132,6 @@ def production_main(
             desc='submitting',
             total=len(runs_not_yet_submitted)
         ):
-            function_to_call_with_job(job)
+            submission(job)
             submitted_at[job.Index] = datetime.utcnow()
         runstatus['submitted_at'] = submitted_at
