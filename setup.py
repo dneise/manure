6a363edaf6c581213fd971fdb789bfc402ed36a1
Dominik Neise <neised@phys.ethz.ch>
2017-09-05 09:38:10 +0200

    ise filelock

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='manure',
-    version='0.1.0',
-    description="Very lightweight helper for producing analyis output e.g. at ISDC",
+    version='0.1.1',
+    description="Very lightweight helper for producing analyis output at ISDC",
     author="Dominik Neise",
     author_email='neised@phys.ethz.ch',
     url='https://github.com/dneise/manure',
     py_modules=['manure'],
     install_requires=[
+        'filelock>=2.0.7'
         'numpy',
         'pandas',
         'pyfact',
